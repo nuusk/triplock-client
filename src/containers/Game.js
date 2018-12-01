038717427dc708d510a5ed55d11f5f1c6e900e18
Poe <piotr.a.ptak@icloud.com>
2018-12-01 12:36:21 +0100

    Game screen prototype

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GameScreen from '../components/GameScreen/GameScreen';
 
 import './Game.scss';
 
@@ -6,14 +7,22 @@ export default class Game extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      grid: [1, 2, 3],
+    };
   }
 
   render() {
+    const { grid } = this.state;
+
     return (
       <section className="game container with-title">
         <h2 className="title">Game</h2>
-        <img src="/mockup-game.png" alt="" />
+        <div className="game-grid">
+          {grid.map(block => (
+            <div className="game-grid__block">asd</div>
+          ))}
+        </div>
       </section>
     );
   }
