879cedd94882e11acefa60979f8ab772b0b1b041
Poe <piotr.a.ptak@icloud.com>
2018-12-01 02:16:35 +0100

    Init env variables

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import AppTitle from './components/AppTitle/AppTitle';
+import Websocket from 'react-websocket';
+// import AppTitle from './components/AppTitle/AppTitle';
 
 import './GlobalStyles.scss';
 import Game from './containers/Game';
 import Buttons from './containers/Buttons';
 import Console from './containers/Console';
 
-export default function App() {
-  return (
-    <div className="app">
-      {/* <AppTitle /> */}
-      <Console />
-      <Game />
-      <Buttons />
-    </div>
-  );
+const WEBSOCKET_URL = 'wss://chatapplication20181201011656.azurewebsites.net/chat';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.handleMessage = this.handleMessage.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+  }
+
+  handleMessage(data) {
+    console.log(`onMessage: ${data}`);
+    console.log(process.env.REACT_APP_MESSAGE_TYPE_TEXT);
+    const { count } = this.state;
+    const result = JSON.parse(data);
+    this.setState({ count: count + result.movement });
+  }
+
+  handleOpen(data) {
+    console.log(`onOpen: ${data}`);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {/* <AppTitle /> */}
+        <Websocket url={WEBSOCKET_URL} onMessage={this.handleMessage} onOpen={this.handleOpen} />
+        <Console />
+        <Game />
+        {/* <p className="balloon from-right">hello</p> */}
+        <Buttons />
+      </div>
+    );
+  }
 }
---
+++ src/components/GameScreen/GameScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const GameScreen = ({ children }) => <div className="game-screen">{children}</div>;
+
+// GameScreen.propTypes = {};
+
+export default GameScreen;
