69ab0e7cb149d99caa43246f86e67f0edbd79758
Poe <piotr.a.ptak@icloud.com>
2018-12-01 16:57:55 +0100

    Add fixed block size
    - for now it's just 140px

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -6,7 +6,7 @@ import './Game.scss';
 const ANIMATION = require('../resources/animations');
 const CARD = require('../resources/cards');
 
-const BLOCK_SIZE = 200;
+const BLOCK_SIZE = 140;
 
 export default class Game extends Component {
   constructor(props) {
@@ -63,7 +63,6 @@ export default class Game extends Component {
         <Spritesheet
           className="my-element__class--style"
           image="/assets/sprites/clemm-idle.png"
-          // style={{ imageRendering: 'pixelated' }}
           widthFrame={32}
           heightFrame={32}
           steps={3}
@@ -71,38 +70,6 @@ export default class Game extends Component {
           direction="forward"
           autoplay
           loop
-          // startAt={10}
-          // endAt={30}
-          // background="http://www.example.com/assets/image.png"
-          backgroundSize="cover"
-          backgroundRepeat="no-repeat"
-          backgroundPosition="center center"
-          getInstance={(spritesheet) => {
-            this.spriteInstance = spritesheet;
-          }}
-          onClick={(spritesheet) => {
-            spritesheet.play();
-          }}
-          onLoopComplete={(spritesheet) => {
-            console.log('onLoopComplete');
-          }}
-          onEachFrame={(spritesheet) => {
-            console.log('onEachFrame');
-          }}
-          onEnterFrame={[
-            {
-              frame: 2,
-              callback: () => {
-                console.log('passed by frame 2');
-              },
-            },
-            {
-              frame: 7,
-              callback: () => {
-                console.log('passed by frame 7');
-              },
-            },
-          ]}
         />
         {/* <div className="player__name">{players[playerId].name}</div>
         <div className="player__animation">{players[playerId].animation}</div> */}
