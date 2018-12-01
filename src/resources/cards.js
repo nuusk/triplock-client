b7f16e9a8623398b84fa88e906a82c3b5262f584
Poe <piotr.a.ptak@icloud.com>
2018-12-01 14:27:56 +0100

    Create game cards

---
+++ src/resources/cards.js
@@ -0,0 +1,27 @@
+module.exports = {
+  ATTACK: {
+    id: 0,
+    name: 'Atak',
+    description: 'Zaatakuj jeden blok przed siebie',
+  },
+  LEFT: {
+    id: 1,
+    name: 'W lewo',
+    description: 'Porusz się o jeden blok w lewo',
+  },
+  RIGHT: {
+    id: 2,
+    name: 'W prawo',
+    description: 'Porusz się o jeden blok w prawo',
+  },
+  UP: {
+    id: 3,
+    name: 'W górę',
+    description: 'Porusz się o jeden blok w górę',
+  },
+  DOWN: {
+    id: 4,
+    name: 'W dół',
+    description: 'Porusz się o jeden blok w dół',
+  },
+};
