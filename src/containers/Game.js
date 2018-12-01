c53b9da24f401c7f2294d1b75ccec6234b5f1d5e
Poe <piotr.a.ptak@icloud.com>
2018-12-01 13:09:51 +0100

    Calculate grid size
    - use fixed BLOCK_SIZE value to calculate block width

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -3,15 +3,30 @@ import GameScreen from '../components/GameScreen/GameScreen';
 
 import './Game.scss';
 
+const BLOCK_SIZE = 200;
+
 export default class Game extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       rows: [1, 1, 1, 1],
-      cols: [1, 1, 1, 1, 1, 1, 1],
+      cols: [1, 1, 1, 1],
       // grid: [1, 2, 3],
     };
+
+    this.calculateWidth = this.calculateWidth.bind(this);
+  }
+
+  calculateWidth() {
+    const { rows } = this.state;
+    const styleObject = {
+      width: `${rows.length * BLOCK_SIZE}px`,
+    };
+
+    console.log(styleObject);
+
+    return styleObject;
   }
 
   render() {
@@ -20,7 +35,7 @@ export default class Game extends Component {
     return (
       <section className="game container with-title">
         <h2 className="title">Game</h2>
-        <div className="game-grid">
+        <div className="game-grid" style={this.calculateWidth()}>
           {cols.map(column => (
             <div className="game-grid__column">
               {rows.map(block => (
